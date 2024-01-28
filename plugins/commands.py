24bc4a92a94fbe519d646bdf60a5a8acd74a23ac
KD Botz <116437604+KDBotz@users.noreply.github.com>
2024-01-28 19:09:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,9 +69,9 @@ async def start(client, message):
             [InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{message.command[1]}")]
         )
         reply_markup = InlineKeyboardMarkup(btn)
-        await client(
+        await client.send_message(
             chat_id=message.from_user.id,
-            caption="**❗️Important**\n\nYou need to join our sponsored channels compulsory\n\nClick below button and **Join our all sponsored channels, then click on the 'Try Again' button** below...\n\nThen you will get the Movie Files...☺️",
+            text="**❗️Important**\n\nYou need to join our sponsored channels compulsory\n\nClick below button and **Join our all sponsored channels, then click on the 'Try Again' button** below...\n\nThen you will get the Movie Files...☺️",
             reply_markup=reply_markup
         )
         return
